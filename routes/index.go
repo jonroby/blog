21c8f0a8a62011083becefc94bfb55f39604e6f5
jonroby <joncaroby@gmail.com>
2018-02-25 18:23:57 -0800

    add postgres connection

--- routes/index.go
+++ routes/index.go
@@ -1,11 +1,47 @@
 package routes
 
 import (
+    "os"
+    "log"
     "net/http"
     "html/template"
+    "database/sql"
+    _ "github.com/lib/pq"
+    "fmt"
 )
 
+type Post struct {
+	Title string
+}
+
+var db *sql.DB
+
 func Index(w http.ResponseWriter, r *http.Request) {
+
+    // Set this in app.yaml when running in production.
+    datastoreName := os.Getenv("POSTGRES_CONNECTION")
+    fmt.Println(datastoreName)
+    var err error
+    db, err = sql.Open("postgres", datastoreName)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    rows, err := db.Query("SELECT * FROM posts")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    defer rows.Close()
+
+    post := Post{}
+    for rows.Next() {
+        err = rows.Scan(&post.Title)
+        if err != nil {
+        	log.Fatal(err)
+        }
+    }
+
     t, _ := template.ParseFiles("templates/index.html")
-    t.Execute(w, nil)
+    t.Execute(w, post)
 }
