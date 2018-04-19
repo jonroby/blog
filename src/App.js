7747da8f3399045d06ef61c94529ea91518fa07d
jonroby <joncaroby@gmail.com>
2018-04-18 23:30:35 -0700

    routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./Navbar.jsx";
 import MyEditor from "./MyEditor.jsx";
 
@@ -8,24 +8,52 @@ import About from "./About.jsx";
 import Posts from "./Posts.jsx";
 import Post from "./Post.jsx";
 import Editor from "./Editor.jsx";
+import Quill from "./Quill.jsx";
 
 import "./App.css";
+
+const home = () => {
+  return (
+    <div>
+      <Navbar loc={"home"} />
+      <Route exact path="/" component={Home} />
+    </div>
+  );
+};
+
+const post = () => {
+  return (
+    <div>
+      <Navbar loc={"post"} />
+      <Route exact path="/posts/:post" component={Post} />
+    </div>
+  );
+};
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <Navbar />
+          <Switch>
+            <Navbar path="*" />
+          </Switch>
+
           <Route exact path="/" component={Home} />
           <Route exact path="/posts" component={Posts} />
-          <Route path="/about" component={About} />
-          <Route path="/editor" component={Editor} />
+          <Route exact path="/posts/:post" component={Post} />
+
+          <Route exact path="/editor" component={Editor} />
+          <Route exact path="/editor/:post" component={Editor} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/quill" component={Quill} />
         </div>
       </BrowserRouter>
     );
   }
 }
+
+// <Route exact path="/posts/:post" component={Post} />
 
 export default App;
 
