8bed73f7912407959ecd9a59e2a516273f654b96
jonroby <joncaroby@gmail.com>
2018-04-22 21:15:51 -0700

    contents

---
+++ src/Contents.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+import './Contents.css';
+
+class Contents extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderContents = contents => {
+    if (!contents) return;
+    return contents.map(c => {
+      return <div>{c}</div>;
+    });
+  }
+
+  render() {
+    const { width, contents } = this.props;
+    return (
+      <div className="contents-container">
+        {/*<div className="contents-fixed" style={{ width }}>
+          <div className="contents">
+            <div>
+              {this.renderContents(contents)}
+            </div>
+          </div>
+        </div>*/}
+      </div>
+    );
+  }
+}
+
+export default Contents;
+
