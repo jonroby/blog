3f5bafda5aee75ac3547141a88f180c665f10cee
jonroby <joncaroby@gmail.com>
2018-04-30 18:44:54 -0700

    fix routes

--- src/App.js
+++ src/App.js
@@ -1,60 +1,36 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./Navbar.jsx";
-import MyEditor from "./MyEditor.jsx";
 
 import Home from "./Home.jsx";
 import About from "./About.jsx";
 import Posts from "./Posts.jsx";
 import Post from "./Post.jsx";
-import Editor from "./Editor.jsx";
 import Quill from "./Quill.jsx";
 
 import "./App.css";
 
-const home = () => {
-  return (
-    <div>
-      <Navbar loc={"home"} />
-      <Route exact path="/" component={Home} />
-    </div>
-  );
-};
-
-const post = () => {
-  return (
-    <div>
-      <Navbar loc={"post"} />
-      <Route exact path="/posts/:post" component={Post} />
-    </div>
-  );
-};
-
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Switch>
-            <Navbar path="*" />
-          </Switch>
+        <BrowserRouter>
+            <div className="App">
+                <Route path="/" component={Navbar} />
 
-          <Route exact path="/" component={Home} />
-          <Route exact path="/posts" component={Posts} />
-          <Route exact path="/posts/:post" component={Post} />
+                <Switch>
+                  <Route exact path="/posts" component={Posts} />
+                  <Route exact path="/posts/:post" component={Post} />
 
-          <Route exact path="/editor" component={Editor} />
-          <Route exact path="/editor/:post" component={Editor} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/quill" component={Quill} />
-        </div>
-      </BrowserRouter>
+                  <Route exact path="/editor" component={Quill} />
+                  <Route exact path="/editor/:post" component={Quill} />
+                  <Route exact path="/about" component={About} />
+
+                  <Route path="*" component={Posts} />
+                </Switch>
+            </div>
+        </BrowserRouter>
     );
   }
 }
 
-// <Route exact path="/posts/:post" component={Post} />
-
 export default App;
-
-// https://www.vanityfair.com/images/trending-horizontal-squiggle.svg
