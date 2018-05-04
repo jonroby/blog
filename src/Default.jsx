8570dd5aac87eaa1917985b5e5ba277485680848
jonroby <joncaroby@gmail.com>
2018-05-04 00:50:13 -0700

    default

---
+++ src/Default.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import './About.css';
+
+class About extends Component {
+    render() {
+        return (
+            <div className="about-screen">
+                <div className="about-screen-middle">
+                    404. No page exists at this location.
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default About;
+
+
+
